96965c5b85e70741e9a98bda0e6c6c2d27d8c2fb
Asghar Nazir <asgharkrk@gmail.com>
2023-01-15 13:22:27 -0500

    array

---
+++ src/class9/ArraysDemo6.java
@@ -0,0 +1,13 @@
+package class9;
+
+public class ArraysDemo6 {
+    public static void main(String[] args) {
+
+     // Create an empty array then store 45 44 33 20 and 10 in it then use a loop
+        // to add all the elements which are present in even indexes and print it after adding them
+
+        // create an array and store 10,20,30,4,5,6,7,80
+        // add all the element which are multiple of 2
+
+    }
+}
