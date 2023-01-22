85507c32317dfa85da67565a2ed6846ffa2ab8e2
Asghar Nazir <asgharkrk@gmail.com>
2023-01-22 10:51:45 -0500

    Class 12

---
+++ src/class12/CarTester.java
@@ -0,0 +1,17 @@
+package class12;
+
+public class CarTester {
+    public static void main(String[] args) {
+        // Create the object of the class
+        Car bmw=new Car();
+        bmw.make="BMW";
+        bmw.model="i7";
+        bmw.year=2023;
+        bmw.color="Grey";
+        bmw.moveForward();
+        bmw.applyBrakes();
+
+
+
+    }
+}
