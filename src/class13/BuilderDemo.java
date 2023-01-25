a1cce3d62673709628276b36ae8a52df4a811860
Asghar Nazir <asgharkrk@gmail.com>
2023-01-24 22:07:53 -0500

    String vs Stringbuilder

--- src/class13/BuilderDemo.java
+++ src/class13/BuilderDemo.java
@@ -2,7 +2,16 @@ package class13;
 
 public class BuilderDemo {
     public static void main(String[] args) {
-        StringBuilder stringBuilder=new StringBuilder("");
+      String state="VA";
+      String state2="VA";
+      String state3="VA";
+        String state4="VA";
+        String state5="VA";
+        String state6="VA";
+        String state7="VA";
+        state7="CA";
+
+        state7="TX";
 
     }
 }
