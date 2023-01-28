c048cfaaf2d3fa1e8b76acd6c63485dc72ab4f1c
Asghar Nazir <asgharkrk@gmail.com>
2023-01-28 11:37:10 -0500

    class 15 Methods

---
+++ src/class15/MethodPracticeTester.java
@@ -0,0 +1,10 @@
+package class15;
+
+public class MethodPracticeTester {
+    public static void main(String[] args) {
+
+        MethodPractice mp=new MethodPractice();
+     boolean isEven=   mp.isEven(15);
+        System.out.println(isEven);
+    }
+}
