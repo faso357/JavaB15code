a590dd3f3207be5422fb81a3ca0795ae625d9741
Asghar Nazir <asgharkrk@gmail.com>
2023-01-28 12:24:12 -0500

    class 15 Methods

---
+++ src/class15/MethodPractice2Tester.java
@@ -0,0 +1,7 @@
+package class15;
+
+public class MethodPractice2Tester {
+    public static void main(String[] args) {
+
+    }
+}
--- src/class15/MethodPracticeTester.java
+++ src/class15/MethodPracticeTester.java
@@ -6,5 +6,12 @@ public class MethodPracticeTester {
         MethodPractice mp=new MethodPractice();
      boolean isEven=   mp.isEven(15);
         System.out.println(isEven);
+        // Break till 12:05
+        // call iseven method for the numbers 100 12 20
+        boolean isEven2=mp.isEven(100);
+        System.out.println(isEven2);
+        System.out.println(mp.isEven(12));
+        System.out.println(mp.isEven(20));
+
     }
 }
