f35b5c87de882fea30f4c171df5ee63d7f8067c0
Asghar Nazir <asgharkrk@gmail.com>
2023-01-29 10:41:17 -0500

    class 16 Tasks

---
+++ src/class16/Task4.java
@@ -0,0 +1,12 @@
+package class16;
+
+public class Task4 {
+    public static void main(String[] args) {
+        /*
+        4)Create a method that will say Hello
+         in different language based on the country
+         that will passed when method is executed
+         */
+
+    }
+}
