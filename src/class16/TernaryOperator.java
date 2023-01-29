98c63d74f1a4e0941b3531930f387789bbad88e0
Asghar Nazir <asgharkrk@gmail.com>
2023-01-29 14:56:38 -0500

    class 16 Access Modifiers

--- src/class16/TernaryOperator.java
+++ src/class16/TernaryOperator.java
@@ -11,7 +11,7 @@ public class TernaryOperator {
         }*/
 
         //System.out.println(number);
-        number=(3>2)?10:20;
+        number=(3>2)?10:20; // shorter way of writing if else conditions
         number=(3>2)?(4>5)?15:65:20;
 
         System.out.println(number);
