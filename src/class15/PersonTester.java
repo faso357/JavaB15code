6dcc670b215171f9bb97b60e696a920faf90c7a1
Asghar Nazir <asgharkrk@gmail.com>
2023-01-29 14:11:47 -0500

    class 16 Access Modifiers

---
+++ src/class15/PersonTester.java
@@ -0,0 +1,14 @@
+package class15;
+
+import class16.Person;
+
+public class PersonTester {
+    public static void main(String[] args) {
+        Person person=new Person();
+      //  System.out.println(person.bankBalance);
+      //  System.out.println(person.address);
+        System.out.println(person.name);
+        person.printTikTokAccount();
+
+    }
+}
