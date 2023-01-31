679d2c76dba648ce520662de29e397de346d87fd
Asghar Nazir <asgharkrk@gmail.com>
2023-01-30 20:16:13 -0500

    class 17 Access Modifiers task3

--- src/class17/Dog.java
+++ src/class17/Dog.java
@@ -3,5 +3,16 @@ package class17;
 public class Dog {
     String name;
     String breed;
+    int age;
+    double weight;
+
+    Dog(String dogName,String dogBreed,int
+            dogAge,double dogWeight){
+        name=dogName;
+        breed=dogBreed;
+        age=dogAge;
+        weight=dogWeight;
+    }
+
 
 }
---
+++ src/class17/DogTester.java
@@ -0,0 +1,10 @@
+package class17;
+
+public class DogTester {
+    public static void main(String[] args) {
+        Dog husky = new Dog("Husky", "Husky the dog",  2, 25.2);
+        Dog bulldog = new Dog("Bulldog", "Bulldog the dog",  3, 27.1);
+        Dog labrador = new Dog("Labrador", "Labrador the dog",  4, 28.0);
+
+    }
+}
