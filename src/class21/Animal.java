0ab631678d1132af3eaa2763a277c6be131dc909
Asghar Nazir <asgharkrk@gmail.com>
2023-02-07 21:12:57 -0500

    class 21 Access Modifiers task3

--- src/class21/Animal.java
+++ src/class21/Animal.java
@@ -4,18 +4,21 @@ public class Animal {
     String name;
     String color="Black";
 
+
 }
 
 class Cat extends Animal{
-    String color="white";
+    //String color="white";
+
     int age=10;
     double weight;
 
-}
-class AnimalTester{
-    public static void main(String[] args) {
-
-        Cat cat=new Cat();
-        System.out.println(cat.color);
+    void printColor(){
+       // String color="Blue";
+        System.out.println(color);
+        System.out.println(this.color);
+        System.out.println(super.color);
     }
+
 }
+
