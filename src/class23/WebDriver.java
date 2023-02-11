0a51b12400bcecd1eb1c49c0aa362b4aab454b18
Asghar Nazir <asgharkrk@gmail.com>
2023-02-11 13:37:07 -0500

    class 22 MethodOverriding

--- src/class23/WebDriver.java
+++ src/class23/WebDriver.java
@@ -1,5 +1,7 @@
 package class23;
 
+import com.sun.security.jgss.GSSUtil;
+
 public class WebDriver {
 
     public void startBrowser(){
@@ -14,6 +16,8 @@ public class WebDriver {
     public  void closeBrowser(){
         System.out.println("closing the browser");
     }
+
+
 }
 
 class Chrome extends WebDriver{
@@ -37,25 +41,48 @@ class Chrome extends WebDriver{
         System.out.println("Closing the Google Chrome");
     }
 }
-class Safari extends WebDriver{
+ class Safari extends WebDriver{
 
     @Override
     public void startBrowser() {
-        super.startBrowser();
+        System.out.println("Opening the Safari Browser");
     }
 
     @Override
     public void openURL() {
-        super.openURL();
+        System.out.println("Opening the Url in Safari");
     }
 
     @Override
     public void testLoginPage() {
-        super.testLoginPage();
+        System.out.println("Testing login page in Safari");
     }
 
     @Override
     public void closeBrowser() {
-        super.closeBrowser();
+        System.out.println("Closing the browser");
+    }
+}
+
+class  FireFox extends WebDriver{
+
+    @Override
+    public void startBrowser() {
+        System.out.println("Opening the FireFox");
+    }
+
+    @Override
+    public void openURL() {
+        System.out.println("Opening the URl in FireFox");
+    }
+
+    @Override
+    public void testLoginPage() {
+        System.out.println("Testing the login page in FireFox");
+    }
+
+    @Override
+    public void closeBrowser() {
+        System.out.println("Closing the Firefox");
     }
 }
