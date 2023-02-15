03378f28ce626567f3d1777925e31b4bb3697243
Asghar Nazir <asgharkrk@gmail.com>
2023-02-14 22:00:20 -0500

    class 25 MethodOverriding

--- src/class25/MovAbleTester.java
+++ src/class25/MovAbleTester.java
@@ -3,5 +3,11 @@ package class25;
 public class MovAbleTester {
     public static void main(String[] args) {
 
+        WashAble [] washAbles={new SmartWatch(),
+                new Inverter(),new Phone()};
+
+        for(WashAble w:washAbles){
+            w.wash();
+        }
     }
 }
