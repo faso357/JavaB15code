ec76c626540e13e30d094e45f9e4aeb568e02707
Asghar Nazir <asgharkrk@gmail.com>
2023-03-05 12:36:31 -0500

    Reading data from Excel class 32

--- src/class32/ExcelFileDemo8.java
+++ src/class32/ExcelFileDemo8.java
@@ -11,7 +11,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-public class ExcelFileDemo7 {
+public class ExcelFileDemo8 {
     public static void main(String[] args) throws IOException {
         String path = "Files/Book1.xlsx";
         FileInputStream fileInputStream = new FileInputStream(path);
@@ -20,17 +20,22 @@ public class ExcelFileDemo7 {
         Files.
          */
         XSSFWorkbook xssfWorkbook=new XSSFWorkbook(fileInputStream);
-        Sheet sheet2=xssfWorkbook.getSheet("Sheet2");
+        Sheet sheet2=xssfWorkbook.getSheet("Sheet1");
         int noOfRows=sheet2.getPhysicalNumberOfRows();
         System.out.println(noOfRows);
         List<Map<String,String>> excelData=new ArrayList<>();
 // 11:53
+        /*
+
+         */
+        Row row0=sheet2.getRow(0);
         for (int i = 1; i < noOfRows; i++) {
           Row row=  sheet2.getRow(i);
             LinkedHashMap<String,String> rowMap=new LinkedHashMap<>();
 
-            rowMap.put("UserName",row.getCell(0).toString());
-            rowMap.put("Password",row.getCell(1).toString());
+            for (int j = 0; j < row.getPhysicalNumberOfCells(); j++) {
+                rowMap.put(row0.getCell(j).toString(),row.getCell(j).toString());
+            }
             excelData.add(rowMap);
         }
 
