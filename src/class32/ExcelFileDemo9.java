bfe0d65e1177fa64278e45d87b2ee6409178ac7a
Asghar Nazir <asgharkrk@gmail.com>
2023-03-05 14:02:00 -0500

    Excel Reader Function Created

--- src/class32/ExcelFileDemo9.java
+++ src/class32/ExcelFileDemo9.java
@@ -9,10 +9,15 @@ import java.util.Map;
 public class ExcelFileDemo9 {
     public static void main(String[] args) throws IOException {
 
+        // Break till 01:52
         List<Map<String,String>> excelData= ExcelReader.read();
-       Map<String,String> firstRow=excelData.get(1);
-       // System.out.println(firstRow.get("salary"));
-        System.out.println(firstRow);
+
+        for (Map<String,String> row:excelData){
+            for (Map.Entry<String,String> entry:row.entrySet()){
+                System.out.println(entry.getKey()+" "+entry.getValue());
+            }
+            System.out.println("**********");
+        }
 
 
     }
