bd0d07abfe22ca5f9e5c967644a46f673063ffb9
Asghar Nazir <asgharkrk@gmail.com>
2023-03-05 12:57:53 -0500

    Excel Reader Function Created

--- src/utils/ExcelReader.java
+++ src/utils/ExcelReader.java
@@ -1,37 +1,30 @@
-package class32;
+package utils;
 
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-public class ExcelFileDemo8 {
-    public static void main(String[] args) throws IOException {
+public class ExcelReader {
+
+    public static List<Map<String,String>> read() throws IOException {
         String path = "Files/Book1.xlsx";
         FileInputStream fileInputStream = new FileInputStream(path);
-        /*
-        A class that we can use to read and write data easily from Excel
-        Files.
-         */
         XSSFWorkbook xssfWorkbook=new XSSFWorkbook(fileInputStream);
         Sheet sheet2=xssfWorkbook.getSheet("Sheet1");
         int noOfRows=sheet2.getPhysicalNumberOfRows();
         System.out.println(noOfRows);
         List<Map<String,String>> excelData=new ArrayList<>();
-
-// 11:53
-        /*
-
-         */
         Row row0=sheet2.getRow(0);
         for (int i = 1; i < noOfRows; i++) {
-          Row row=  sheet2.getRow(i);
+            Row row=  sheet2.getRow(i);
             LinkedHashMap<String,String> rowMap=new LinkedHashMap<>();
 
             for (int j = 0; j < row.getPhysicalNumberOfCells(); j++) {
@@ -40,7 +33,7 @@ public class ExcelFileDemo8 {
             excelData.add(rowMap);
         }
 
-        System.out.println(excelData);
+         return excelData;
 
     }
 }
